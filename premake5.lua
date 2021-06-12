84d5cd7e8baf6cc2bcc878ce72eadfafd8def1a2
canturkmen <31515770+canturkmen@users.noreply.github.com>
2021-06-12 15:02:31 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -25,15 +25,15 @@ project "GLFW"
 
 		files
 		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+		  "src/win32_init.c",
+		  "src/win32_joystick.c",
+		  "src/win32_monitor.c",
+		  "src/win32_time.c",
+		  "src/win32_thread.c",
+		  "src/win32_window.c",
+		  "src/wgl_context.c",
+		  "src/egl_context.c",
+		  "src/osmesa_context.c"
 		}
 
 		defines 
